c2166f5706d4a0a293252715cfdc0c2473f81a36
Allan Khelifi <khelifiallan@gmail.com>
2025-03-10 16:34:29 +0100

    Add client app and ts-config

---
+++ client/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Route, Routes } from "react-router-dom";
+
+export default function App() {
+    return (
+        <div>
+            <div className="App">
+                <Routes>
+                </Routes>
+            </div>
+        </div>
+    );
+}
